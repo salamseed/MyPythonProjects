a793895eb04100aac98c3de57959788ca2e97f88
unknown <salamava66@gmail.com>
2023-04-24 12:58:56 +0530

    added

--- ecommerce1/ecommerce1/cartapp/views.py
+++ ecommerce1/ecommerce1/cartapp/views.py
@@ -50,3 +50,15 @@ def cart_item_delete(request,product_id):
     cart_item = CartItem.objects.get(product=product, cart=cart)
     cart_item.delete()
     return redirect('cartapp:cart_detail')
+def payment(request,total=0,counter=0,cart_items=None):
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart,active=True)
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            counter += cart_item.quantity
+    except ObjectDoesNotExist:
+        pass
+    return render(request,'payment.html',dict(cart_items=cart_items,total=total,counter=counter))
+def payment_success(request):
+    return render(request,'success.html')
